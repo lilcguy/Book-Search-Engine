0808aad870eb65d853b69179bd3897d36082eaaa
unknown <thecbradshaw89@gmail.com>
2023-03-30 03:57:29 -0500

    setting up types, queries, and mutations in typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Book { 
+    bookId: #not to be confused with _id
+    authors:
+    description:
+    title:
+    image:
+    link:
+
+}
+
+type User {
+    _id: 
+    username:
+    email:
+    bookCount:
+    savedBooks: 
+
+}
+
+type Auth {
+    token:
+    user:
+
+
+}
+
+
+type Query { #queries that can be used are put into this object
+    #me : which returns a User type
+}
+
+type Mutation { #mutations that can be used, i.e. changes to data, are put into this object.
+# login: Accepts an email and password as parameters; returns an Auth type.
+
+#addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+
+# saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
+
+#removeBook: Accepts a book's bookId as a parameter; returns a User type.
+
+}
+
+
+
+`;
+
+module.exports = typeDefs;
